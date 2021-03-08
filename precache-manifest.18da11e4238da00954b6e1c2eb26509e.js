185c7707f95f3bde3bfa2c7ab127dafa36a63240
vikas6208 <vikas62081@gmail.com>
2021-03-08 11:08:31 +0530

    Updates

---
+++ precache-manifest.18da11e4238da00954b6e1c2eb26509e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082b7d46d557a692aec5ecbc8181529e",
+    "url": "/newTodos/index.html"
+  },
+  {
+    "revision": "e3e622a5d499a6dd63b4",
+    "url": "/newTodos/static/css/main.19ea13fd.chunk.css"
+  },
+  {
+    "revision": "20f9e19d9180b9367753",
+    "url": "/newTodos/static/js/2.cc25e0ef.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/newTodos/static/js/2.cc25e0ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e622a5d499a6dd63b4",
+    "url": "/newTodos/static/js/main.238a9300.chunk.js"
+  },
+  {
+    "revision": "72d45ec18be4b2105aa0",
+    "url": "/newTodos/static/js/runtime-main.2fd3f55c.js"
+  }
+]);
